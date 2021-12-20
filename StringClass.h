d94c7fdfbf7d80238f760e92a9d22a80ac5624df
eynavbe <93534494+eynavbe@users.noreply.github.com>
2021-12-20 09:02:09 +0200

    Add files via upload

---
+++ StringClass.h
@@ -0,0 +1,15 @@
+typedef int bool;
+#define True 1
+#define False 0
+
+int gematricWord(char*);
+
+bool checksIfGematricCharIs0 (char *);
+
+char* gematriaSequences(char*, char*);
+
+char hatbash(char);
+
+char* atbashSequences(char*, char*);
+
+char* anagramSequences(char*, char*);
